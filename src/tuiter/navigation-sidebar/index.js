1530a46327a93eadfddb127927a71a16c24c6271
mingyue21 <myyyyywang@gmail.com>
2023-02-25 21:54:12 -0800

    a6 modify navigation bar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,42 +8,112 @@ const NavigationSidebar = () => {
     const active = paths[2];
     return (
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
+            <a className="list-group-item">
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-twitter"></i>
+                    </div>
+                </div>
+            </a>
             <Link to="/tuiter/home" className={`list-group-item
                     ${active === 'home'?'active':''}`}>
-                Home
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-house-door-fill"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Home
+                    </div>
+                </div>
             </Link>
             <Link to="/tuiter/explore" className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
-                Explore
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-hash"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Explore
+                    </div>
+                </div>
             </Link>
             <Link to="/" className={`list-group-item`}>
-                Labs
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-hammer"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Labs
+                    </div>
+                </div>
             </Link>
             <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-                Notifications
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-bell-fill"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Notifications
+                    </div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                Messages
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-envelope-fill"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Messages
+                    </div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-                Bookmarks
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-bookmark-fill"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Bookmarks
+                    </div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-                Lists
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-list-ul"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Lists
+                    </div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-                Profile
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-person-fill"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        Profile
+                    </div>
+                </div>
             </a>
             <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-                More
+                <div className="row">
+                    <div className="col-1">
+                        <i className="bi bi-three-dots"></i>
+                    </div>
+                    <div className="col-9 d-none d-xl-block">
+                        More
+                    </div>
+                </div>
             </a>
+            <a href="#" className="btn btn-primary rounded-pill mt-2">Tuit</a>
         </div>
     );
 };
